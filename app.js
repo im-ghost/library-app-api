129b19edcc5fb1882cab7affd6ce92581fd31efb
Adetonwa Richard <culestfrosh@gmail.com>
2022-10-15 11:06:28 +0100

    Removed the port in the app file

--- app.js
+++ app.js
@@ -16,7 +16,4 @@ app.use("/", graphqlHTTP({
   schema,
   graphiql: true
 }))
-const port = process.env.PORT || 5000
-app.listen(port, ()=> {
-  console.log("Listening on port 5000")})
 module.exports = app;
